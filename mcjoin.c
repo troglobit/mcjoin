4c24a1fe20cadfa07b5f17ba557dca953c1396bb
Joachim Nilsson <troglobit@gmail.com>
2016-03-05 23:08:16 +0100

    Replace [-f FIRST] [-n NUM] with FIRST+NUM syntax, default to join 225.1.2.3
    This patch replaces the [-f FIRST] [-n NUM] command line options with a
    more intuitive GROUP+NUM argument syntax.  This simplifies the code a
    lot and folds the parsing and validation into the same path as the rest
    of the code.
    
    Also, if no command line arguments are given mcjoin now defaults to join
    the group 225.1.2.3
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- mcjoin.c
+++ mcjoin.c
@@ -29,6 +29,7 @@
 #include <unistd.h>
 
 #define DEFAULT_IFNAME "eth0"
+#define DEFAULT_GROUP  "225.1.2.3"
 
 #define DEBUG(fmt, ...) { if (debug)  printf(fmt, ## __VA_ARGS__); fflush(stdout); }
 #define PRINT(fmt, ...) { if (!quiet) printf(fmt, ## __VA_ARGS__); fflush(stdout); }
@@ -51,17 +52,15 @@ int sock = 0, count = 0;
 static int usage(int code)
 {
 	fprintf(stderr,
-		"\nUsage: %s [dqvh] [-i IFNAME] [-f FIRST] [-n NUM] [GROUP0 .. GROUPN]\n"
+		"\nUsage: %s [dhqv] [-i IFNAME] [GROUP0 .. GROUPN | GROUP+NUM]\n"
 		"\n"
 		"Options:\n"
 		"  -d           Debyg output\n"
-		"  -f GROUP     First Mulitcast group, e.g. 225.1.2.3\n"
-		"  -n N         Total number of multicast groups, e.g. 50\n"
+		"  -h           This help text\n"
 		"  -i IFNAME    Interface to use for multicast groups, default %s\n"
 		"  -q           Quiet mode\n"
 		"  -r N         Do a join/leave every N seconds\n"
 		"  -v           Display program version\n"
-		"  -h           This help text\n"
 		"\n"
 		"Mandatory arguments to long options are mandatory for short options too\n"
 		"Bug report address: %-40s\n\n", __progname, DEFAULT_IFNAME, program_bug_address);
@@ -103,7 +102,7 @@ static int join_group(char *iface, char *group)
 		fprintf(stderr, "%s: \"%s\" invalid group address\n", __func__, group);
 		return 1;
 	}
-	DEBUG("GROUP %#x (%s)\n", mreqn.imr_multiaddr.s_addr, group);
+	DEBUG("GROUP %#x (%s)\n", ntohl(mreqn.imr_multiaddr.s_addr), group);
 
 	if (setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreqn, sizeof(mreqn)) < 0) {
 		fprintf(stderr, "%s: IP_ADD_MEMBERSHIP: %m\n", __func__);
@@ -118,99 +117,89 @@ static int join_group(char *iface, char *group)
 int main(int argc, char *argv[])
 {
 	int total = 0, restart = 0;
-	int i, c, start_group;
-	char iface[40], start[16], *group;
-	struct in_addr start_in_addr;
+	int i, c;
+	char iface[IFNAMSIZ], *group, *groups[100];
+	struct in_addr addr;
 
 	/* Default interface
 	 * XXX - Should be the first, after lo, in the list at /proc/net/dev, or
 	 * XXX - Iterate over /sys/class/net/.../link_mode */
 	strncpy(iface, DEFAULT_IFNAME, sizeof(iface));
 
-	while ((c = getopt(argc, argv, "df:n:i:r:qvh?")) != EOF) {
+	while ((c = getopt(argc, argv, "di:r:qvh")) != EOF) {
 		switch (c) {
-		case 'd':	/* --debug */
+		case 'd':
 			debug = 1;
 			break;
 
-		case 'f':
-			start_in_addr.s_addr = 0;
-			if (inet_aton(optarg, &start_in_addr) == 0) {
-				perror("Not a valid IP-address for first Multicast group.\n");
-				return 1;
-			}
-			strncpy(start, optarg, sizeof(start));
-			start_group = ntohl(start_in_addr.s_addr);
-			DEBUG("MCSTART: %s, %#x (HOST:%#x)\n", start, start_in_addr.s_addr, start_group);
-			break;
-
-		case 'n':	/* number-of-groups */
-			total = atoi(optarg);
-			DEBUG("GROUPS: %d\n", total);
-			break;
+		case 'h':
+			return usage(0);
 
 		case 'i':
 			strncpy(iface, optarg, sizeof(iface));
 			DEBUG("IFACE: %s\n", iface);
 			break;
 
-		case 'q':	/* --quiet */
+		case 'q':
 			quiet = 1;
 			break;
 
-		case 'r':	/* --restart */
+		case 'r':
 			restart = atoi(optarg);
 			DEBUG("RESTART: %d\n", restart);
 			if (restart < 1)
 				restart = 1;
 			break;
 
-		case 'v':	/* --version */
+		case 'v':
 			printf("%s\n", program_version);
 			return 0;
 
-		case 'h':
-		case '?':
 		default:
-			return usage(0);
+			return usage(1);
 		}
 	}
 
-	/* At least one argument needed. */
-	if (argc < 2)
-		return usage(1);
+	if (optind == argc)
+		groups[total++] = DEFAULT_GROUP;
+
+	/*
+	 * mcjoin group+num
+	 * mcjoin group0 group1 group2
+	 */
+	for (i = optind; i < argc; i++) {
+		int j, num = 1;
+		char *pos;
+
+		group = argv[i];
+		pos = strchr(group, '+');
+		if (pos) {
+			*pos = 0;
+			num = atoi(&pos[1]);
+		}
+
+		for (j = 0; j < num; j++) {
+			if (!inet_aton(group, &addr)) {
+				fprintf(stderr, "%s is not a valid IPv4 multicast group\n", group);
+				return usage(1);
+			}
+
+			DEBUG("Adding group %s (0x%04x) to list ...\n", group, ntohl(addr.s_addr));
+			groups[total++] = strdup(group);
+
+			/* Next group ... if any */
+			addr.s_addr = htonl(ntohl(addr.s_addr) + 1);
+			group = inet_ntoa(addr);
+		}
+	}
 
 	while (1) {
-		if (!total) {
-			for (i = optind; i < argc; i++) {
-				start_in_addr.s_addr = 0;
-				if (inet_aton(argv[i], &start_in_addr) == 0) {
-					fprintf(stderr, "Invalid IPv4 group %s: %m\n", argv[i]);
-					return 1;
-				}
-				start_group = ntohl(start_in_addr.s_addr);
-				start_in_addr.s_addr = htonl(start_group);
-				group = inet_ntoa(start_in_addr);
+		for (i = 0; i < total; i++) {
+			group = groups[i];
 
-				DEBUG("Trying to join %#x (%s)\n", start_in_addr.s_addr, group);
-				if (join_group(iface, group)) {
-					/* Bailing out. */
-					DEBUG("Bailing out...\n");
-					return 1;
-				}
-			}
-		} else {
-			for (i = 0; i < total; i++) {
-				start_in_addr.s_addr = htonl(start_group + i);
-				group = inet_ntoa(start_in_addr);
-
-				DEBUG("Trying to join %#x (%s)\n", start_in_addr.s_addr, group);
-				if (join_group(iface, group)) {
-					/* Bailing out. */
-					DEBUG("Bailing out...\n");
-					return 1;
-				}
-			}
+			DEBUG("Trying to join %s\n", group);
+			if (join_group(iface, group))
+				return 1;
 		}
 
 		if (!restart)
