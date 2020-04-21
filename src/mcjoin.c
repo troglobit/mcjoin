d620db8096fd894e7c69a63c3091a53079e2aba7
Joachim Nilsson <troglobit@gmail.com>
2020-04-21 08:31:46 +0200

    Increase MAX number of multicast groups: 250 -> 2048
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/mcjoin.c
+++ src/mcjoin.c
@@ -33,7 +33,7 @@
 #include "log.h"
 
 #define BUFSZ           100
-#define MAX_NUM_GROUPS  250
+#define MAX_NUM_GROUPS  2048
 #define DEFAULT_GROUP   "225.1.2.3"
 #define DEFAULT_PORT    1234
 #define MAGIC_KEY       "Sender PID "
