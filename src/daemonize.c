troglobit/mcjoin

commit 50f7c3047ba8bf759de7df248512ccdbc33eb1bc

Minor, refactor
Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

src/daemonize.c
@@ -1,0 +1,67 @@
+/*
+ * Copyright (c) 2018-2020  Joachim Wiberg <troglobit@gmail.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <fcntl.h>
+#include <unistd.h>
+
+int daemonize(void)
+{
+	int rc = 0;
+
+	/* Going to background ... */
+	if ((rc = fork())) {
+		if (rc == -1)
+			return -1;
+		_exit(0);
+	}
+
+	/* Starta a new session */
+	if ((rc = setsid())) {
+		if (rc == (pid_t)-1)
+			return -1;
+		_exit(0);
+	}
+
+	/*
+	 * Old double-fork trick to prevent us from reacquiring
+	 * any controlling terminal.
+	 */
+	if ((rc = fork())) {
+		if (rc == -1)
+			return -1;
+		_exit(0);
+	}
+
+	/* Neutral directory in case of unmounts */
+	rc |= chdir("/");
+
+	/* Going dark */
+	rc |= close(0) | close(1) | close(2);
+
+	/* Redirect stdio to /dev/null */
+	if (open("/dev/null", O_RDWR) != 0 ||
+	    dup(0) == -1 || dup(0) == -1 || rc)
+		_exit(1);
+
+	return 0;
+}
+
+/**
+ * Local Variables:
+ *  indent-tabs-mode: t
+ *  c-file-style: "linux"
+ * End:
+ */

src/Makefile.am
@@ -1,4 +1,4 @@
 bin_PROGRAMS      = mcjoin
-mcjoin_SOURCES    = mcjoin.c addr.c addr.h log.c log.h
+mcjoin_SOURCES    = mcjoin.c addr.c addr.h daemonize.c log.c log.h
 mcjoin_CFLAGS     = -W -Wall -Wextra -std=gnu99
 mcjoin_CPPFLAGS   = -D_GNU_SOURCE

src/mcjoin.c
@@ -19,7 +19,6 @@
 #include "config.h"
 
 #include <errno.h>
-#include <fcntl.h>		/* open() on macOS */
 #include <getopt.h>
 #include <libgen.h>
 #include <poll.h>
@@ -29,7 +28,6 @@
 #include <string.h>
 #include <sys/time.h>
 #include <sys/resource.h>
-#include <unistd.h>
 
 #include "addr.h"
 #include "log.h"
@@ -84,6 +82,8 @@ struct gr groups[MAX_NUM_GROUPS];
 
 char iface[IFNAMSIZ + 1];
 int num_joins = 0;
+
+extern int daemonize(void);
 
 
 static int alloc_socket(inet_addr_t group)
@@ -689,34 +689,10 @@ int main(int argc, char *argv[])
 		groups[group_num++].group = strdup(DEFAULT_GROUP);
 
 	if (!foreground) {
-		int rc = 0;
-
-		/* Going to background ... */
-		if (fork())
-			_exit(0);
-
-		/* Starta a new session */
-		if (setsid() == (pid_t)-1)
+		if (daemonize()) {
+			printf("Failed backgrounding: %s", strerror(errno));
 			_exit(1);
-
-		/*
-		 * Old double-fork trick to prevent us from reacquiring
-		 * any controlling terminal.
-		 */
-		if (fork())
-			_exit(0);
-
-
-		/* Neutral directory in case of unmounts */
-		rc |= chdir("/");
-
-		/* Going dark */
-		rc |= close(0) | close(1) | close(2);
-
-		/* Redirect stdio to /dev/null */
-		if (open("/dev/null", O_RDWR) != 0 ||
-		    dup(0) == -1 || dup(0) == -1 || rc)
-			_exit(1);
+		}
 
 		log_syslog = 1;
 

commit 22ce804e7ca5f40e57c3b9604df93c98e7195b50

daemonize: fix setsid conditional after refactoring
A bug was introduced in 50f7c3047ba8bf759de7df248512ccdbc33eb1bc, where mcjoin
would not daemonise correctly. setsid returns a non-zero value during normal
operation, and that causes mcjoin to exit instead.

src/daemonize.c
@@ -28,12 +28,9 @@ int daemonize(void)
 		_exit(0);
 	}
 
-	/* Starta a new session */
-	if ((rc = setsid())) {
-		if (rc == (pid_t)-1)
-			return -1;
-		_exit(0);
-	}
+	/* Start a new session */
+	if (setsid() == (pid_t)-1)
+		return -1;
 
 	/*
 	 * Old double-fork trick to prevent us from reacquiring